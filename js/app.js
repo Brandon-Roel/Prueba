bdfa25d343fc36ae4fa15d3c93252205dcb86f7c
Practicante Datos Maestros <prac.datosmaestros@gonac.com.mx>
2025-06-26 20:23:10 -0600

    Boton para instalar

--- js/app.js
+++ js/app.js
@@ -17,3 +17,30 @@ if ('serviceWorker' in navigator) {
     .then(() => console.log('✔ Service Worker registrado'))
     .catch((error) => console.log('❌ Error en el Service Worker', error));
 }
+
+// Código para mostrar el botón de instalar PWA
+let deferredPrompt;
+const btnInstalar = document.getElementById('btnInstalar');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault(); // Evita que se muestre el diálogo automático
+  deferredPrompt = e; // Guarda el evento
+  btnInstalar.style.display = 'inline-block'; // Muestra el botón
+});
+
+btnInstalar.addEventListener('click', () => {
+  btnInstalar.style.display = 'none'; // Oculta el botón
+
+  if (deferredPrompt) {
+    deferredPrompt.prompt(); // Muestra el diálogo de instalación
+
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('Usuario aceptó la instalación');
+      } else {
+        console.log('Usuario rechazó la instalación');
+      }
+      deferredPrompt = null;
+    });
+  }
+});
